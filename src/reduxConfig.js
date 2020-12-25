8ca26c31f490062b32d42b1c6fb52fa4d4981616
KishanSinghChauhan <kishansingh705446@gmail.com>
2020-12-25 20:55:16 +0530

    add redux persist

--- src/reduxConfig.js
+++ src/reduxConfig.js
@@ -2,15 +2,23 @@ import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import rootReducer from "./redux/reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "allInfo",
+  storage: storage,
+  whitelist: ["allInfo"], 
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
 
 const loggerMiddleware = createLogger();
 
 const configureStore = (preloadedState) => {
-  return createStore(
-    rootReducer,
+  return persistStore(createStore(
+    pReducer,
     preloadedState,
     applyMiddleware(thunkMiddleware, loggerMiddleware)
-  );
+  ));
 }
 
 export default configureStore;
